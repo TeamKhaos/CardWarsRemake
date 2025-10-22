c1bd69bb98284bd13d203b7686a63e203a7a77fc
Mateo Garzon <137083225+WesitosFsa@users.noreply.github.com>
2025-10-21 21:00:25 -0500

    Cartas en Godot
    Casi todos sabemos amar

--- scripts/ManejoCarta.gd
+++ scripts/ManejoCarta.gd
@@ -35,10 +35,10 @@ func empezar_a_arrastrar(carta):
 
 func dejar_de_arrastrar():
 	carta_siend_arrastrada.scale = Vector2(1.05, 1.05)
-	var carta_ranura_encontrada = raycast_check_carta()
+	var carta_ranura_encontrada = raycast_check_carta_ranura()
 	if carta_ranura_encontrada and not carta_ranura_encontrada.carta_en_ranura:
 		carta_siend_arrastrada.global_position = carta_ranura_encontrada.global_position
-		carta_siend_arrastrada.get_node("Area2D").monitoring = false
+		carta_siend_arrastrada.get_node("Area2D/CollisionShape2D").disabled = true
 		carta_ranura_encontrada.carta_en_ranura = true
 	carta_siend_arrastrada = null
 	
@@ -71,7 +71,7 @@ func resaltar_carta(carta, sosteniendo):
 		carta.scale = Vector2(1, 1)
 		carta.z_index = 1
 		
-func raycast_check_carta():
+func raycast_check_carta_ranura():
 	var space_state = get_world_2d().direct_space_state
 	var parametros = PhysicsPointQueryParameters2D.new()
 	parametros.position = get_global_mouse_position()
@@ -85,7 +85,7 @@ func raycast_check_carta():
 	
 	return  null
 
-func raycast_check_carta_ranura():
+func raycast_check_carta():
 	var space_state = get_world_2d().direct_space_state
 	var parametros = PhysicsPointQueryParameters2D.new()
 	parametros.position = get_global_mouse_position()
