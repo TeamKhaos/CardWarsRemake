68cb023a779a41cc8e9e3c4933b77cb8cae33b33
Yuverly Hidokun <yuverlyverdezoto@gmail.com>
2025-10-21 22:30:25 -0500

    mano del jugador programada

---
+++ scripts/mano_jugador.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+const conteo_mano = 2
+const carta_escena_dir = "res://scenes/card.tscn"
+var mano_jugador = []
+const carta_ancho = 200
+const mano_y_posicion = 890
+var center_screen_x
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	center_screen_x = get_viewport().size.x/2
+	var carta_escena = preload(carta_escena_dir)
+	for i in range(conteo_mano):
+		var nueva_carta = carta_escena.instantiate()
+
+		$"../ManejoCarta".add_child(nueva_carta)
+		nueva_carta.name = "Carta"
+		añadir_carta_mano(nueva_carta)
+
+func añadir_carta_mano(carta):
+	mano_jugador.insert(0, carta)
+	actulizar_posicion_mano()
+	
+func actulizar_posicion_mano():
+	for i in range(mano_jugador.size()):
+		var nueva_posicion = Vector2(calcular_carta_posicion(i), mano_y_posicion)
+		var carta = mano_jugador[i]
+		animar_carta_a_posicion(carta, nueva_posicion) 
+
+func calcular_carta_posicion(index):
+	var total_ancho = (mano_jugador.size() - 1) * carta_ancho
+	var x_offset = center_screen_x + index * carta_ancho - total_ancho / 2
+	return x_offset
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func animar_carta_a_posicion(carta, nueva_posicion):
+	var tween = get_tree().create_tween()
+	tween.tween_property(carta, "position", nueva_posicion, 0.1)
