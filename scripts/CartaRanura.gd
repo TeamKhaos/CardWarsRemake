9e5302050f17af07d7c7315f80856a33e8a8f8bc
Mateo Garzon <mateogarzonfsa@gmail.com>
2025-10-23 22:24:54 -0500

    centro de mesa refactor

--- scripts/CartaRanura.gd
+++ scripts/CartaRanura.gd
@@ -1,3 +1,12 @@
 extends Node2D
 
 var carta_en_ranura = false
+
+@export var is_player_one: bool = true
+func _ready() -> void:
+	var tamano_ventana = get_viewport().size
+	if is_player_one:
+		self.position = Vector2(tamano_ventana.x * 0.55, tamano_ventana.y * 0.5)
+	else:
+		self.position = Vector2(tamano_ventana.x * 0.45, tamano_ventana.y * 0.5)
+	
